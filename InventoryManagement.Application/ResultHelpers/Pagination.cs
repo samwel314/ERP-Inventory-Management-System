8791243c8d95caee14400b2fe81787f3b3daa51b
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-03-12 17:09:17 +0200

    feat : add Pagination model

---
+++ InventoryManagement.Application/ResultHelpers/Pagination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.Application.ResultHelpers
+{
+    public class Pagination <T>
+    {
+        public IEnumerable<T> Items { get; set; } = null!;
+        public int Next { get;}
+        public int Previous { get;}     
+        public int TotalPages { get;}
+        public int Size { get; }    
+        public int pageSize { get; } 
+        public int pageNumber { get; }
+        public Pagination(int totalCount , int pageSize , int pageNumber )
+        {
+            this.pageSize = pageSize < 5 ? 5 : pageSize;    
+            this.TotalPages = (int)Math.Ceiling( totalCount / (double)  this.pageSize ) ;
+            this.pageNumber = pageNumber < 1  || pageNumber > this.TotalPages ? 1 : pageNumber; 
+            this.Previous = this.pageNumber == 1 ? 0 : this.pageNumber - 1;
+            this.Next = this.pageNumber >= this.TotalPages ? 0 : this.pageNumber + 1;
+        }
+    }
+}
--- InventoryManagement.Application/ResultHelpers/Result.cs
+++ InventoryManagement.Application/ResultHelpers/Result.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace InventoryManagement.Application
+namespace InventoryManagement.Application.ResultHelpers
 {
     public class Result<T>
     {
