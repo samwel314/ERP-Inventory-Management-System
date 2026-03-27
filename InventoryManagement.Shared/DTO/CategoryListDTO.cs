977618cb7df92c5b4e6218d60ee357169c2c0fce
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-03-27 02:30:14 +0200

    feat(category) :add CategoryListDTO

---
+++ InventoryManagement.Shared/DTO/CategoryListDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace InventoryManagement.Shared.DTO
+{
+    public class CategoryListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int ProductsCount { get; set; } 
+    }
+}
