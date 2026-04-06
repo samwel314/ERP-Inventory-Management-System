7cb9a0500686097e0d3f8990167a474b4da49b9a
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-06 20:57:18 +0200

    feat(Warehouses): add warehouses path endpoints and nav path

--- InventoryManagement.UI/UrlsPath.cs
+++ InventoryManagement.UI/UrlsPath.cs
@@ -5,6 +5,7 @@
         public const string BaseUrl = "https://localhost:7120/api/";
         public const string Products = "products";
         public const string Categories = "categories";
+        public const string Warehouses = "warehouses"; 
         public const string Host = "https://localhost:7120";
     }
 }
