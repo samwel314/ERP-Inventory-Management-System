7d9468c08f202d275391e471884bc795c2caa383
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-07 15:02:03 +0200

    feat(tests): add unit tests for category entity domain logic

---
+++ InventoryManagement.UnitTests/Domain/CategoryTests.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagement.UnitTests.Domain
+{
+    public class CategoryTests
+    {
+        [Fact]
+        public void CanBeDeleted_IfProductsCountIsZero_ShouldReturnTrue()
+        {
+            var category = new Category("TestOne"); 
+            var result = category.CanBeDeleted(0);
+            Assert.True(result); 
+        }
+        [Fact]
+        public void CanBeDeleted_IfProductsCountIsMoreThanZero_ShouldReturnFalse()
+        {
+            var category = new Category("TestOne");
+            var result = category.CanBeDeleted(10);
+            Assert.False(result);
+        }
+    }
+}
