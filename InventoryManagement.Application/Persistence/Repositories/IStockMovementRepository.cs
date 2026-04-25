3a201d6ee97b9a23814c0789ac95432e695d7c95
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-26 01:59:28 +0300

    feat: stock movement repo

---
+++ InventoryManagement.Application/Persistence/Repositories/IStockMovementRepository.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Domain.Entities;
+
+namespace InventoryManagement.Application.Persistence.Repositories
+{
+    public interface IStockMovementRepository
+    {
+        Task CreateAsync(StockMovement stockMovement, CancellationToken ct = default);
+        IQueryable<StockMovement> GetAllProductMovement(Guid productId);
+        IQueryable<StockMovement> GetAllWarehouseMovement(int warehouseId);
+    }
+
+}
---
+++ InventoryManagement.Infrastructure/Persistence/Repositories/StockMovementRepository.cs
@@ -0,0 +1,36 @@
+﻿using InventoryManagement.Application.Persistence.Repositories;
+using InventoryManagement.Domain.Entities;
+using InventoryManagement.Infrastructure.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+namespace InventoryManagement.Infrastructure.Persistence.Repositories
+{
+    public class StockMovementRepository : IStockMovementRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StockMovementRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task CreateAsync(StockMovement stockMovement, CancellationToken ct = default)
+        {
+            await _db.StockMovements.AddAsync(stockMovement, ct);
+        }
+
+        public IQueryable<StockMovement> GetAllProductMovement(Guid productId)
+        {
+            return _db.StockMovements
+                .AsNoTracking()
+                .Where(sm => sm.ProductId == productId)
+                .OrderByDescending(sm => sm.CreatedAt); 
+        }
+        public IQueryable<StockMovement> GetAllWarehouseMovement(int warehouseId)
+        {
+            return _db.StockMovements
+                .AsNoTracking()
+                .Where(sm => sm.FromWarehouseId == warehouseId || sm.ToWarehouseId == warehouseId)
+                .OrderByDescending(sm => sm.CreatedAt);
+        }
+    }
+}
